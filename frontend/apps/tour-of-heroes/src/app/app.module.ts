2ec14af5ca8578ebc58d6ebeb7d26c9f419671c7
Lucas Valente <lucas.valente9999@gmail.com>
2020-10-13 10:00:07 -0300

    Atualizado a lista de heróis.

--- frontend/apps/tour-of-heroes/src/app/app.module.ts
+++ frontend/apps/tour-of-heroes/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule }     from '@angular/forms';
 
 import { AppComponent }    from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   providers: [
 
