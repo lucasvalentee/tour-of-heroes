03b4ef30a34a9c939a9e156aae616fafb8413850
Lucas Valente <lucas.valente9999@gmail.com>
2020-10-13 17:33:59 -0300

    Criado os menus para navegação no app.

--- frontend/apps/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ frontend/apps/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,17 +12,11 @@ import { MessageService } from './../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
 
-	constructor(private heroService: HeroService, private messageService: MessageService) { }
+	constructor(private heroService: HeroService) { }
 
 	ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: selected hero = { id: ${hero.id}, name: ${hero.name} }`);
   }
 
   getHeroes(): void {
